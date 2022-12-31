a6506e64ff6766f630bdb7d94127b097383e7190
mekrm <mills.ryan.011@gmail.com>
2022-12-31 17:19:34 +0000

    basic javascript functions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+function sing() {
+    console.log("Laughing on the outside...");
+    console.log("crying on the inside");
+    console.log("cuz I'm so in love.. with youuuuuu!!!");
+}
+
+sing();
+
+function greet(name) {
+    console.log(`HI ${name}`);
+}
+
+greet('Endo');
