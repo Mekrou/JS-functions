352b0d09b4f3bb902661bd73b02516ac4dcdfcbc
mekrm <mills.ryan.011@gmail.com>
2022-12-31 20:03:45 +0000

    lexical scope example

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-function sing() {
-    console.log("Laughing on the outside...");
-    console.log("crying on the inside");
-    console.log("cuz I'm so in love.. with youuuuuu!!!");
+// function sing() {
+//     console.log("Laughing on the outside...");
+//     console.log("crying on the inside");
+//     console.log("cuz I'm so in love.. with youuuuuu!!!");
+// }
+
+// sing();
+
+// function greet(name) {
+//     console.log(`HI ${name}`);
+// }
+
+// greet('Endo');
+
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
 }
 
-sing();
 
-function greet(name) {
-    console.log(`HI ${name}`);
+// lexical scope
+function bankRobbery() {
+	const hereos = ['Spiderman', 'Superman', 'Batwomen'];
+	function cryForHelp() {
+		for (let hero of hereos) {
+			console.log(`please help us ${hero}`);
+        }
+    }
+    cryForHelp();
 }
 
-greet('Endo');
+bankRobbery();
